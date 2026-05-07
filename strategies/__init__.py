6a96d72c24d86445f6b3da805acd390a8819e464
TechSolute <pharshiesammy@gmail.com>
2026-05-07 22:18:57 +0100

    Update __init__.py

--- strategies/__init__.py
+++ strategies/__init__.py
@@ -1,1 +1,15 @@
+# src/strategies/__init__.py
+"""
+Super Joint 8 Strategy Engines
+"""
+from .trend_rider import TrendRider
+from .squeeze_rocket import SqueezeRocket
+from .mean_reversion import MeanReversion
+from .smc_engine import SMCEngine
+from .vwap_reversion import VWAPReversion
+from .funding_fade import FundingFade
+from .liq_sweep import LiqSweep
+from .onchain_macro import OnChainMacro
 
+__all__ = ['TrendRider', 'SqueezeRocket', 'MeanReversion', 'SMCEngine', 
+           'VWAPReversion', 'FundingFade', 'LiqSweep', 'OnChainMacro']

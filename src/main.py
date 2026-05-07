59591990d5ab2b485ad5044ebf6a9b28ac5475d1
TechSolute <pharshiesammy@gmail.com>
2026-05-07 22:22:44 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
 
 # External libs
 import ccxt
-import pandas_ta as ta
+import pandas_ta_classic as ta
 from discord_webhook import DiscordWebhook
 
 # Configure logging

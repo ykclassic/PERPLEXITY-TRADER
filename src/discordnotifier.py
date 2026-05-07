82a24216a5e91d2188e6b363f24ae9b775ae091f
TechSolute <pharshiesammy@gmail.com>
2026-05-07 22:03:44 +0100

    Update discordnotifier.py

--- src/discordnotifier.py
+++ src/discordnotifier.py
@@ -1,8 +1,12 @@
-from discord_webhook import DiscordWebhook
 import os
+from discord_webhook import DiscordWebhook
 
-def send_signal(embed: str):
+def send_signal(embed: str) -> Any:
+    """Send Discord webhook."""
     webhook_url = os.getenv('DISCORD_WEBHOOK')
+    if not webhook_url:
+        raise ValueError("DISCORD_WEBHOOK env var required")
+    
     webhook = DiscordWebhook(url=webhook_url, content=embed)
     response = webhook.execute()
     return response
